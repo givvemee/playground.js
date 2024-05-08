972290e3ea517ac90f04cdea7be35a833e9f6b43
givvemee <givvemeee@gmail.com>
2024-05-08 10:20:40 +0900

    zz

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
+console.log(1);
 
-const apiCall = () => {
-  fetch(url, {
+fetch(
+  "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc",
+  {
     headers: {
       accept: "application/json",
       Authorization: `bearer ${process.env.API_KEY}`,
     },
+  }
+)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    console.log(process.env.API_KEY);
   })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      console.log(process.env.API_KEY);
-    })
-    .catch((err) => JSON.stingify(err));
-};
-
-apiCall();
+  .catch((err) => JSON.stingify(err));
